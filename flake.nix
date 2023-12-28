f9dac641cf1d09c44bef8312e62f405fd5eb6db5
Vid Visocnik <vid.visocnik@paymentology.com>
2023-12-28 16:06:37 +0100

    pushed changes

--- flake.nix
+++ flake.nix
@@ -1,29 +1,41 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  description = "Nix Development Flake for your package";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { self, nixpkgs, flake-utils }:
+
+    flake-utils.lib.eachDefaultSystem
+      (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowBroken = true;
-            allowUnsupportedSystem = true;
-          };
-        };
-
-        node = pkgs.nodejs-18_x;
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python310;
+        pythonPackages = python.pkgs;
       in
       {
-        devShell = pkgs.mkShell {
-          buildInputs = [
+        devShells.default = pkgs.mkShell {
+          name = "your_package";
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+          buildInputs = with pythonPackages; [
+            pkgs.nodePackages.pyright
+            pkgs.poetry
             pkgs.act
             pkgs.just
-            pkgs.poetry
-            pkgs.python311
+            setuptools
+            wheel
+            venvShellHook
           ];
+          venvDir = ".venv";
+          src = null;
+          postVenv = ''
+            unset SOURCE_DATE_EPOCH
+          '';
+          postShellHook = ''
+            unset SOURCE_DATE_EPOCH
+            unset LD_PRELOAD
+
+            PYTHONPATH=$PWD/$venvDir/${python.sitePackages}:$PYTHONPATH
+          '';
         };
-      }
-    );
+      });
 }
