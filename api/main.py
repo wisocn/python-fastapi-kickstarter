b71f183056e9763b4c63814992132296c289851c
Vid Visocnik <vid.visocnik@paymentology.com>
2023-12-28 14:36:31 +0100

    moves to uvicorn and async python

---
+++ api/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
