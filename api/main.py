c461601f1001a1d9c2174989a0f7218d4c93f459
Vid Visocnik <vid.visocnik@paymentology.com>
2024-01-16 22:04:44 +0100

    WIP, database works!

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
 import uvicorn
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from api.components.db import postgres
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await postgres.init_db()
+    yield
+    print("clean up lifespan")
+
+# initialise the database at app startup
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
@@ -11,15 +20,12 @@ async def root():
 async def add_new_todo():
     return {"message": "Hello World"}
  
-@app.get("/todo/:id")
-async def get_todo():
-    return {"message": "Hello World"}
+@app.get("/todo/{id}")
+async def get_todo(id: str):
+    return {"message": "Hello World with id" + id}
 
 @app.put("/todo/:id")
 async def update_todo():
-    return {"message": "Hello World"}
-
-async def delete_todo():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
